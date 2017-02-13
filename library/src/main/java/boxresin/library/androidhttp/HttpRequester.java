cede09612b7c77033aea5727e349aa377ad9ebed
Minsuk Eom <boxresin@gmail.com>
2017-02-13 13:33:56 +0900

    Designed base of the library.

--- library/src/main/java/boxresin/library/androidhttp/HttpRequester.java
+++ library/src/main/java/boxresin/library/androidhttp/HttpRequester.java
@@ -1,6 +1,54 @@
 package boxresin.library.androidhttp;
 
+/**
+ * A class to send HTTP requests.
+ */
 public class HttpRequester
 {
+	private String url; // URL to requst
+	private String method; // HTTP method
 
+	/**
+	 * Return the URL to request.
+	 * @return The URL to request
+	 */
+	public String getUrl()
+	{
+		return url;
+	}
+
+	/**
+	 * Sets the URL to request.
+	 * @param url The URL to request
+	 */
+	public void setUrl(String url)
+	{
+		this.url = url;
+	}
+
+	/**
+	 * Return HTTP method to request.
+	 * @return HTTP method as String
+	 */
+	public String getMethod()
+	{
+		return method;
+	}
+
+	/**
+	 * Sets HTTP method
+	 * @param method HTTP method as String
+	 */
+	public void setMethod(String method)
+	{
+		this.method = method;
+	}
+
+	/**
+	 * Send HTTP Request to web server.
+	 */
+	public HttpResponse request()
+	{
+		return null;
+	}
 }
---
+++ library/src/main/java/boxresin/library/androidhttp/HttpResponse.java
@@ -0,0 +1,52 @@
+package boxresin.library.androidhttp;
+
+/**
+ * A class representing HTTP response.
+ * You can get information in response(ex. HTTP version of a web server, HTTP status code, body of a message etc) via this class.
+ */
+public class HttpResponse
+{
+	/**
+	 * Preventing instantiation of HttpResponse with constructor.
+	 */
+	HttpResponse()
+	{
+
+	}
+
+	/**
+	 * Return HTTP version of the web server.
+	 * @return HTTP version as String (ex. "HTTP/1.1")
+	 */
+	public String getHttpVersion()
+	{
+		return null;
+	}
+
+	/**
+	 * Return HTTP status code of a response message.
+	 * @return HTTP status code as int. (ex. 404)
+	 */
+	public int getStatusCode()
+	{
+		return 0;
+	}
+
+	/**
+	 * Return a description of HTTP status code.
+	 * @return description of HTTP status code as String. (ex. "Not Found")
+	 */
+	public String getStatusCodeMessage()
+	{
+		return null;
+	}
+
+	/**
+	 * Return content of a response message. It can be an HTML document or JSON-formatted data.
+	 * @return body of a response message as String.
+	 */
+	public String getBody()
+	{
+		return null;
+	}
+}
